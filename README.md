ba801121f1c2b9366d99640f3e81bfc45b4685a2
KaranBengani <KaranBengani@github.com>
2020-10-05 11:46:26 +0530

    trial

---
+++ README.md
@@ -0,0 +1,10 @@
+# ChefsFiesta
+This is a repository for the website of our upcoming event Chefs Fiesta.
+
+# Tech Used
+
+* HTML
+* CSS
+* JS
+* AOS 
+* Bootstrap
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 // This is a sample js file will be used later when registration form is ready
+var i = 0;
+var txt = "Made with 💓 by Codechef SRM";
+var speed = 250;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
